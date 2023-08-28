d0f4081c1252b2c1bd1fd6126b781c56eacfb397
ren276 <sandeshv414@gmail.com>
2023-08-28 21:37:34 +0530

    day 2

---
+++ conditionals_and_loops.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+
+
+
+
+
+//     int num =7;
+
+//     if (num % 2==0){
+//         cout << "even" <<endl;
+//     }
+//     else {
+//         cout << "odd" << endl;
+//     }
+}
